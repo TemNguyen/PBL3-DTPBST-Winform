3572393bb625efdb5123d45982434f62217b4da4
thanhlong965 <thanhlong9654147@example.com>
2021-04-28 13:56:04 +0700

    updateDB

---
+++ PBL3_DanTaPhaiBietSuTa/DTO/GameProcess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBL3_DanTaPhaiBietSuTa.DTO
+{
+    public class GameProcess
+    {
+        [Key]
+        public int GameProcessID { get; set; }
+
+        public int StageID { get; set; }
+        public int Point { get; set; }
+        public int UserID { get; set; }
+        [ForeignKey("UserID")]
+        public virtual UserInfo UserInfo { get; set; }
+    }
+}
---
+++ PBL3_DanTaPhaiBietSuTa/DTO/Video.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBL3_DanTaPhaiBietSuTa.DTO
+{
+    public class Video
+    {
+        [Key]
+        public int VideoID { get; set; }
+        public string VideoName { get; set; }
+        public string VideoLink { get; set; }
+    }
+}
