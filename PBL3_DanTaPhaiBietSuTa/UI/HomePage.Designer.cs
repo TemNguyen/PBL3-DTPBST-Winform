73d95450aaddb993fa4702eca10a3827c97f6d05
jthanh8144 <thanhvo618@gmail.com>
2021-04-30 23:47:26 +0700

    Add noti form

--- PBL3_DanTaPhaiBietSuTa/UI/HomePage.Designer.cs
+++ PBL3_DanTaPhaiBietSuTa/UI/HomePage.Designer.cs
@@ -42,17 +42,17 @@ namespace PBL3_DanTaPhaiBietSuTa.UI
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.btnInfo = new PBL3_DanTaPhaiBietSuTa.OvalPictureBox();
-            this.btnHelp = new PBL3_DanTaPhaiBietSuTa.OvalPictureBox();
-            this.btnSetting = new PBL3_DanTaPhaiBietSuTa.OvalPictureBox();
             this.label9 = new System.Windows.Forms.Label();
             this.gbHelp = new System.Windows.Forms.GroupBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.btnInfo = new PBL3_DanTaPhaiBietSuTa.OvalPictureBox();
+            this.btnHelp = new PBL3_DanTaPhaiBietSuTa.OvalPictureBox();
+            this.btnSetting = new PBL3_DanTaPhaiBietSuTa.OvalPictureBox();
             this.gbIntro.SuspendLayout();
+            this.gbHelp.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnInfo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnHelp)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnSetting)).BeginInit();
-            this.gbHelp.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnPlayNow
@@ -70,7 +70,7 @@ namespace PBL3_DanTaPhaiBietSuTa.UI
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(500, 85);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.Size = new System.Drawing.Size(68, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "logo here";
             // 
@@ -123,7 +123,7 @@ namespace PBL3_DanTaPhaiBietSuTa.UI
             this.label8.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(145, 224);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(169, 25);
+            this.label8.Size = new System.Drawing.Size(218, 32);
             this.label8.TabIndex = 24;
             this.label8.Text = "Nguyễn Duy Thịnh";
             // 
@@ -133,7 +133,7 @@ namespace PBL3_DanTaPhaiBietSuTa.UI
             this.label7.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(166, 188);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(129, 25);
+            this.label7.Size = new System.Drawing.Size(162, 32);
             this.label7.TabIndex = 23;
             this.label7.Text = "Võ Văn Thành";
             // 
@@ -143,7 +143,7 @@ namespace PBL3_DanTaPhaiBietSuTa.UI
             this.label6.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(164, 153);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(136, 25);
+            this.label6.Size = new System.Drawing.Size(173, 32);
             this.label6.TabIndex = 22;
             this.label6.Text = "Lê Thanh Long";
             // 
@@ -153,7 +153,7 @@ namespace PBL3_DanTaPhaiBietSuTa.UI
             this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(27, 123);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(232, 25);
+            this.label5.Size = new System.Drawing.Size(298, 32);
             this.label5.TabIndex = 21;
             this.label5.Text = "Nhóm sinh viên thực hiện:";
             // 
@@ -163,7 +163,7 @@ namespace PBL3_DanTaPhaiBietSuTa.UI
             this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(133, 87);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(203, 25);
+            this.label4.Size = new System.Drawing.Size(261, 32);
             this.label4.TabIndex = 20;
             this.label4.Text = "GVHD: Lê Thị Mỹ Hạnh";
             // 
@@ -173,7 +173,7 @@ namespace PBL3_DanTaPhaiBietSuTa.UI
             this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(27, 55);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(413, 25);
+            this.label3.Size = new System.Drawing.Size(529, 32);
             this.label3.TabIndex = 19;
             this.label3.Text = "Đề tài: App game lịch sử \"Dân ta phải biết sử ta\"";
             // 
@@ -183,9 +183,45 @@ namespace PBL3_DanTaPhaiBietSuTa.UI
             this.label2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(108, 16);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(272, 37);
+            this.label2.Size = new System.Drawing.Size(339, 46);
             this.label2.TabIndex = 18;
             this.label2.Text = "PBL 3: Đồ án lập trình";
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.White;
+            this.label9.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label9.Location = new System.Drawing.Point(0, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(440, 35);
+            this.label9.TabIndex = 26;
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gbHelp
+            // 
+            this.gbHelp.BackColor = System.Drawing.Color.White;
+            this.gbHelp.Controls.Add(this.button2);
+            this.gbHelp.Controls.Add(this.label9);
+            this.gbHelp.Location = new System.Drawing.Point(306, 230);
+            this.gbHelp.Name = "gbHelp";
+            this.gbHelp.Size = new System.Drawing.Size(440, 334);
+            this.gbHelp.TabIndex = 27;
+            this.gbHelp.TabStop = false;
+            this.gbHelp.Text = "groupBox1";
+            this.gbHelp.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(406, -2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 35);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // btnInfo
             // 
@@ -226,42 +262,6 @@ namespace PBL3_DanTaPhaiBietSuTa.UI
             this.btnSetting.TabStop = false;
             this.btnSetting.Click += new System.EventHandler(this.btnSetting_Click);
             // 
-            // label9
-            // 
-            this.label9.BackColor = System.Drawing.Color.White;
-            this.label9.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.label9.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.label9.Location = new System.Drawing.Point(0, 0);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(440, 35);
-            this.label9.TabIndex = 26;
-            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // gbHelp
-            // 
-            this.gbHelp.BackColor = System.Drawing.Color.White;
-            this.gbHelp.Controls.Add(this.button2);
-            this.gbHelp.Controls.Add(this.label9);
-            this.gbHelp.Location = new System.Drawing.Point(306, 230);
-            this.gbHelp.Name = "gbHelp";
-            this.gbHelp.Size = new System.Drawing.Size(440, 334);
-            this.gbHelp.TabIndex = 27;
-            this.gbHelp.TabStop = false;
-            this.gbHelp.Text = "groupBox1";
-            this.gbHelp.Visible = false;
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(406, -2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(35, 35);
-            this.button2.TabIndex = 26;
-            this.button2.Text = "X";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // HomePage
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
@@ -282,10 +282,10 @@ namespace PBL3_DanTaPhaiBietSuTa.UI
             this.Text = "HomePage";
             this.gbIntro.ResumeLayout(false);
             this.gbIntro.PerformLayout();
+            this.gbHelp.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.btnInfo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnHelp)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnSetting)).EndInit();
-            this.gbHelp.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
