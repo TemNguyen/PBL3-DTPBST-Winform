7578dad2667d5b28c22182c0574045362ad6b01c
Nguyễn Duy Thịnh <temthinh30012001@gmail.com>
2021-05-06 07:43:00 +0700

    merge UI 6/5

--- PBL3_DanTaPhaiBietSuTa/Program.cs
+++ PBL3_DanTaPhaiBietSuTa/Program.cs
@@ -17,7 +17,7 @@ namespace PBL3_DanTaPhaiBietSuTa
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Play());
+            Application.Run(new HomePage());
         }
     }
 }
