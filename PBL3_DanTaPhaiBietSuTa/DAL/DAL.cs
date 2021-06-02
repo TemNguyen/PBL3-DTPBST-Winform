15367f5c83b56f06dd95f8c0a880c1a927ab372e
thanhlong965 <thanhlong9654147@example.com>
2021-06-02 20:06:49 +0700

    update

--- PBL3_DanTaPhaiBietSuTa/DAL/DAL.cs
+++ PBL3_DanTaPhaiBietSuTa/DAL/DAL.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Security.Cryptography;
 namespace PBL3_DanTaPhaiBietSuTa
 {
     public class DAL : InterfaceDAL
@@ -205,6 +205,18 @@ namespace PBL3_DanTaPhaiBietSuTa
                 UserInfo user = db.UserInfos.Where(s => s.UserID == userID).FirstOrDefault();
                 return user;
             }
+        }
+        public string Hash(string str)
+        {
+            MD5 mh = MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(str);
+            byte[] hash = mh.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
         }
 
 
